c36b289e1ad0c1e68458b9402362c1033f717b0c
Matt Schaff <mschaff@societyforscience.org>
2017-05-31 12:49:25 -0400

    binning spikes by 5 ms bins

--- sorting_metrics.m
+++ sorting_metrics.m
@@ -12,19 +12,43 @@ function sorting_metric_output = sorting_metrics()
         h = msgbox('Select the folder that contains the KiloSort *.npy output files');
         pause(1.5);
         delete(h);
-        data_directory = uigetdir()
+        data_directory = uigetdir();
         spike_times = readNPY([data_directory slash 'spike_times.npy']);
         spike_clusters = readNPY([data_directory slash 'spike_clusters.npy']);
+     % Load rez.mat
+        load([data_directory slash 'rez.mat']);
+        sampling_rate = rez.ops.fs; % in Hz
+        disp(sampling_rate);
      % Convert data into spikes by bin by cluster(cell)
-        for i = 1:max(spike_clusters) % 1 to num clusters
+        % intialize variables
+        bin_size = 5e-3; % milliseconds
+        max_time = double(max(spike_times))/double(sampling_rate);
+        num_bins = round(max_time/bin_size) + 1; % add a bin to be safe
+        %loop
+        spikes_by_bin = [];
+        disp('Binning cluster spike times by 5 ms bins.');
+        parfor_progress(max(spike_clusters));
+        clusters = zeros(max(spike_clusters),1);
+        parfor i = 1:max(spike_clusters) % 1 to num clusters
             % get spike times of the cell
-            cluster_spikes = spike_times(find(spike_clusters==i));
+            cluster_spikes = double(spike_times(find(spike_clusters==i)))./double(sampling_rate); % now in seconds
+            clusters(i) = numel(cluster_spikes);
             if numel(cluster_spikes) == 0
                 continue;
             end
-            disp(numel(cluster_spikes));
             % collect by bin
+            parfor_progress;
+            for j=1:num_bins
+                bin = j*bin_size;
+                bin_prev = (j-1)*bin_size;
+                spikes_by_bin(i,j) = numel(cluster_spikes(cluster_spikes <= bin & cluster_spikes > bin_prev));
+            end    
         end
+        spikes_by_bin = spikes_by_bin(find(clusters>0), :);
+        active_clusters = find(clusters>0);
+        disp(active_clusters);
+        parfor_progress(0);
+        save('spikes_by_bin.mat', 'spikes_by_bin');
 %% Cross-correlation matrix with a threshold
         %  false positives & negatives
         %  max R with a sliding lag, max lag 50 ms
