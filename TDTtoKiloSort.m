61ff8c397dfa8d9d82ea381643cb695d0c532310
marsalad <marsalad@users.noreply.github.com>
2017-06-13 18:38:53 -0400

    removed unnecessary functions and cleaned up comments

--- TDTtoKiloSort.m
+++ TDTtoKiloSort.m
@@ -1,60 +1,39 @@
 function TDTtoKiloSort(tank, block, varargin)
 
-useGPU = 1; % do you have a GPU? Kilosorting 1000sec of 32chan simulated data takes 55 seconds on gtx 1080 + M2 SSD.
-
-fpath    = 'C:\work\Spikes\TDTEx1\'; % where on disk do you want the simulation? ideally and SSD...
+% specify location to store data files for this simulation
+fpath = 'C:\work\Spikes\TDTtoKiloSort\';
 if ~exist(fpath, 'dir'); mkdir(fpath); end
 
-% This part adds paths
 addpath(genpath('C:\work\KiloSort-master')) % path to kilosort folder
 addpath(genpath('C:\work\phy-master')) % path to npy-matlab scripts
-pathToYourConfigFile = 'C:\work\TDT';
+pathToYourConfigFile = 'C:\work\TDTtoKiloSort'; % path to the config file
 
-% Run the configuration file, it builds the structure of options (ops)
-run(fullfile(pathToYourConfigFile, 'config_TDTData.m'))
+% run the config file to build the structure of options (ops)
+run(fullfile(pathToYourConfigFile, 'config.m'))
 
-data = TDT2mat(tank, block, varargin);
-% raw data
-fs = data.streams.NRaw.fs;
+if ops.GPU, gpuDevice(1); end %#ok initialize GPU
+tic; % start timer
+
+% format raw TDT data
+data = TDT2mat(tank, block, varargin); % extract data from tank file
 rData = data.streams.NRaw.data; % n x m matrix, n = num electrodes, m = timesteps
+ops.fs = data.streams.NRaw.fs; % set sampling rate
 
-fidW     = fopen(fullfile(fpath, 'sim_binary.dat'), 'w');
+% store formatted data
+fidW = fopen(fullfile(fpath, 'sim_binary.dat'), 'w');
 fwrite(fidW, rData, 'int16');
-fclose(fidW); % all done
+fclose(fidW);
 
-% This part runs the normal Kilosort processing on the simulated data
-[rez, DATA, uproj] = preprocessData(ops); % preprocess data and extract spikes for initialization
-rez                = fitTemplates(rez, DATA, uproj);  % fit templates iteratively
-rez                = fullMPMU(rez, DATA);% extract final spike times (overlapping extraction)
+% process formatted data with KiloSort
+[rez, DATA, uproj] = preprocessData(ops); % preprocess data and extract spikes
+rez = fitTemplates(rez, DATA, uproj); % fit templates iteratively
+rez = fullMPMU(rez, DATA); % extract final spike times (overlapping extraction)
+rez = merge_posthoc2(rez); % use KiloSort's automerge functionality
 
-% save python results file for Phy
-rezToPhy(rez, fpath);
+rezToPhy(rez, fpath); % save python results file for Phy
+save(fullfile(fpath,  'rez.mat'), 'rez', '-v7.3'); % save matlab results file
+delete(ops.fproc); % remove temporary file
 
-fprintf('Kilosort took %2.2f seconds vs 72.77 seconds on GTX 1080 + M2 SSD \n', toc)
-
-% now fire up Phy and check these results. There should still be manual
-% work to be done (mostly merges, some refinements of contaminated clusters). 
-% AUTO MERGES 
-% after spending quite some time with Phy checking on the results and understanding the merge and split functions, 
-% come back here and run Kilosort's automated merging strategy. This block
-% will overwrite the previous results and python files. Load the results in
-% Phy again: there should be no merges left to do (with the default simulation), but perhaps a few splits
-% / cleanup. On realistic data (i.e. not this simulation) there will be drift also, which will usually
-% mean there are merges left to do even after this step. 
-% Kilosort's AUTO merges should not be confused with the "best" merges done inside the
-% benchmark (those are using the real ground truth!!!)
-
-rez = merge_posthoc2(rez);
-benchmark_simulation(rez, fullfile(fpath, 'eMouseGroundTruth.mat'));
-
-% save python results file for Phy
-rezToPhy(rez, fpath);
-
-% save and clean up
-% save matlab results file for future use (although you should really only be using the manually validated spike_clusters.npy file)
-save(fullfile(fpath,  'rez.mat'), 'rez', '-v7.3');
-
-% remove temporary file
-delete(ops.fproc);
-
-end   
+% final display of total runtime
+fprintf('KiloSort took %2.2f minutes to run \n', floor(toc/0.6)/100)
+end
