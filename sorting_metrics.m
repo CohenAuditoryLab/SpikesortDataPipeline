1831d79cac8e29c772b720c01d050d10eb8a4cc5
Matt Schaff <mschaff@societyforscience.org>
2017-06-13 17:56:56 -0400

    drift measure: slope of firing rate (over 30s) over session

--- sorting_metrics.m
+++ sorting_metrics.m
@@ -236,6 +236,32 @@ function sorting_metric_output = sorting_metrics()
             
             disp('Saved cluster autocorrelograms.');
         
- %% avg firing rate changing over time
- 
+ %% drift measure: slope of firing rate (over 30s) over session
+    % bin spikes by minute
+    firing_window = 30; % seconds
+    num_small_bins = 1/bin_size*firing_window;
+    num_windows = round(size(spikes_by_bin,2)/num_small_bins);
+    firing_rates = zeros(size(spikes_by_bin,1), num_windows);
+    line_slopes = zeros(1,num_windows);
+    x = firing_window:firing_window:num_windows*firing_window;
+%     figure; hold on;
+    for k=1:size(spikes_by_bin,1) %loop through cells
+        for i=1:num_windows
+            firing_rates(k,i) = sum(spikes_by_bin(k,(i-1)*num_small_bins+1:i*num_small_bins))/firing_window; % in Hz
+        end
+        %fit line
+        P = polyfit(x,firing_rates(k,:),1);
+        line_slopes(k) = P(1);
+%         plot(x,firing_rates(k,:));
+%         yfit = P(1)*x+P(2);
+%         plot(x,yfit,'r-.');
+    end
+%     hold off;
+%     figure;
+    f = bar(line_slopes);
+    a = gca; a.XTick = 1:30;xticklabels(active_clusters);
+    xlabel('Clusters');
+    ylabel('Slope of Firing Rate over Time (Hz/s)');
+    title('Slope of Firing Rate over Time (30s bins)');
+    saveas(f, [new_directory slash 'drift__frate_slope.png']);
 end
