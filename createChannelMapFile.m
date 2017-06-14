6c923d8c8d42b439d74155da5f05fba4f684f4aa
marsalad <marsalad@users.noreply.github.com>
2017-06-14 12:33:39 -0400

    created file to make and save chanMap

---
+++ createChannelMapFile.m
@@ -0,0 +1,16 @@
+function createChannelMapFile(fpath)
+%createChannelMapFile  Create a chanMap file for a KiloSort simulation.
+
+Nchannels   = 96;
+connected   = true(Nchannels, 1);
+chanMap     = 1:Nchannels;
+chanMap0ind = chanMap - 1;
+xcoords     = ones(Nchannels,1); % all channels in same x-coord
+ycoords     = ones(Nchannels,1); % all channels in same y-coord
+kcoords     = ones(Nchannels,1); % grouping of channels (i.e. tetrode groups)
+
+fs = 25000; % sampling frequency
+
+save(fullfile(fpath, 'chanMap.mat'), ...
+    'chanMap','connected', 'xcoords', 'ycoords', 'kcoords', 'chanMap0ind','fs')
+end
