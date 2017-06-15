cf8481ee0ebfc7a0950229b041267f6a63b5ee39
bkarpowicz3 <brianna.karpowicz@gmail.com>
2017-06-15 11:40:30 -0400

    channel-by-channel processing, output 1 cell array

--- mat2waveclus.m
+++ mat2waveclus.m
@@ -1,31 +1,58 @@
-function mat2waveclus
-
+function mat2waveclus(filename)
+%select path for data
+disp('Select the path for your .mat data');
 path = uigetdir();
 cd(path);
-load data_for_bk;
+
+disp(['Loading ' filename]);
+load(filename);
 
 %define data
 data_temp = data.streams.NRaw.data;
+r = size(data_temp, 1);
+    
+%select path for wave_clus
+disp('Select the path for wave_clus');
+waveclus = uigetdir();
+cd(waveclus);
+
+%define sampling rate
 sr = data.streams.NRaw.fs;
 index = data.snips.eBxS.ts;
-data = data_temp;
-param.channels = size(data_temp, 1);
 
-%save to mat file
-name = 'eBxS_to_wave_clus';
-ext = '.mat';
-save(strcat(name, ext), 'data', 'index', 'sr', 'param');
+%define matrix for storing all outputs
+allClusters = cell(1,8);
 
-%make cell with name of files to use 
-file = {strcat(name, ext)};
+for n = 1:r
+    data = data_temp(n, :);
+    
+    name = strcat('channel', num2str(n));
+    ext = '.mat';
+    
+    %save .mat file
+    save(strcat(name, ext), 'data', 'index', 'sr');
+    
+    %make cell with name of files to use
+    file = {strcat(name, ext)};
+    
+    disp(['Processing channel ', num2str(n), ' of ', num2str(r)]);
+    
+    %Get spikes 
+    Get_spikes(file);
+    %outputs filename_spikes.mat
+    
+    %get clusters
+    Do_clustering(strcat(name, '_spikes', ext));
+    
+    %load clustering results
+    load(strcat('times_', name, ext));
+        
+    %add clustering results to all matrix
+    allClusters{n} = cluster_class;
+end
 
-%get spikes 
-Get_spikes(file, 'par', param);
-
-%outputs filename_spikes.mat
-%get clusters 
-Do_clustering(strcat(name, '_spikes', ext));
-
-%eventual output is times_filename.mat
+disp('Saving final data matrix...');
+save(strcat('clusters_', filename, '.mat'), 'allClusters');
+disp('Done!');
 
 end
