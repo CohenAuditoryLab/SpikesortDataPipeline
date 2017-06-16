6eb02c55e8e3b1d80cdd05d116120bb66d8be806
bkarpowicz3 <brianna.karpowicz@gmail.com>
2017-06-16 18:11:43 -0400

    genpath change

--- mat2waveclus.m
+++ mat2waveclus.m
@@ -18,18 +18,18 @@ if 7~=exist(data_directory, 'dir') mkdir(data_directory); end
 cd(data_directory);
 
 %define data
-data_temp = spikes;
+data_temp = spikesML;
 r = size(data_temp, 1);
-    
+
 %select path for wave_clus
 disp('Select the path for wave_clus');
 waveclus = uigetdir();
 %cd(waveclus);
-addpath(waveclus);
+addpath(genpath(waveclus));
 %define sampling rate, timestamps, and parameters
-sr = fs;
+sr = 24410;
 index = (0:size(data_temp,2)-1)/sr;
-param.stdmin = 3.5; 
+param.stdmin = 3.5;
 param.segments_length = 10;
 param.to_plot_std = 2;
 param.detect_fmax = 7000;
@@ -51,7 +51,7 @@ for n = 1:r
     
     disp(['Processing channel ', num2str(n), ' of ', num2str(r)]);
     
-    %Get spikes 
+    %Get spikes
     Get_spikes(file, 'par', param, 'parallel', true);
     %outputs filename_spikes.mat
     
@@ -60,7 +60,7 @@ for n = 1:r
     
     %load clustering results
     load(strcat('times_', name, ext));
-        
+    
     %add clustering results to all matrix
     allClusters{n} = cluster_class;
 end
