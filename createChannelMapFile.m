aef8fc018aedba432149e85958f97d2340fae5a6
marsalad <marsalad@users.noreply.github.com>
2017-06-30 15:19:42 -0400

    updated coords, removed fs, added Nchannel param

--- createChannelMapFile.m
+++ createChannelMapFile.m
@@ -1,16 +1,24 @@
-function createChannelMapFile(fpath)
+function createChannelMapFile(fpath,Nchannels)
 %createChannelMapFile  Create a chanMap file for a KiloSort simulation.
 
-Nchannels   = 64;
+if nargin<2
+    Nchannels = 64; 
+end
+
 connected   = true(Nchannels, 1);
 chanMap     = 1:Nchannels;
 chanMap0ind = chanMap - 1;
-xcoords     = ones(1,Nchannels); % all channels in same x-coord
-ycoords     = 1:Nchannels;       % channels spaced evenly along y-axis
+xcoords     = []; % all channels in same x-coord
+ycoords     = []; % channels spaced evenly along y-axis
 kcoords     = ones(1,Nchannels); % grouping of channels (i.e. tetrode groups)
 
-fs = 24414; % sampling frequency
+%%%%
+% Form for running MultiBrush arrays, channel position is unkown
+% xcoords = zeros(nchannels,1);
+% ycoords = 50 * [1:nchannels];
+% kcoords = ones(nchannels,1);
+%%%%
 
 save(fullfile(fpath, 'chanMap.mat'), ...
-    'chanMap','connected', 'xcoords', 'ycoords', 'kcoords', 'chanMap0ind','fs')
+    'chanMap','connected', 'xcoords', 'ycoords', 'kcoords', 'chanMap0ind')
 end
