9f7ba52092a59ddd88999b9c74c32208854b0023
bkarpowicz3 <brianna.karpowicz@gmail.com>
2017-07-06 12:29:21 -0400

    GUI features updated to include closer look at metrics (paths idealized for sorting computer)

--- NeuronDecisions.m
+++ NeuronDecisions.m
@@ -1,23 +1,32 @@
 function NeuronDecisions(varargin)
-
+%% Handle variable argument numbers for automated vs. manual options
 if nargin < 2 
+    wave_or_kilo = questdlg('Is your data from WaveClus or KiloSort?',...
+        'Wave or Kilo?','wave','kilo','wave');
     disp('Select data path')
     [fname,fpath] = uigetfile();
     fpath = fullfile(fpath,fname);
     display(fpath);
     disp('Select save path')
+    %save path should be base folder containing kilosort and waveclus
+    %subfolders
     new_directory = uigetdir();
 else 
     fpath = varargin{1};
     new_directory = varargin{2};
+    wave_or_kilo = varargin{3};
 end
+%% Load data and initialize figure/table/table callback 
     
 output = load(fpath);
-g = output.standard_output;
-f = figure();
+g = output.g; %output.standard_output;
+f = figure('Color', 'White');
+set(f, 'name','Neuron Decisions');
 pos = f.Position;
 ax = axes('Visible', 'off');
-t = uitable(f);
+t = uitable(f, 'CellSelectionCallback', @cellSelect);
+
+%% UI for selecting clusters and button for submitting 
 
 clusters = unique(g(:,1));
 d = cell(length(clusters), 2);
@@ -26,23 +35,55 @@ for j = 1:length(clusters)
     d(j, 1:2) = {num2str(clusters(j)), true};
 end
 
-t.Data = d;
-
+figure(f);
 button = uicontrol('Visible', 'on', 'style','pushbutton','units', ...
     'pixels','string','Submit');
 set(button, 'Callback', {@callback});
 button.Position(4) = 30;
-button.Position(2) = 0;
-button.Position(3) = 119;
-uicontrol('Style','text', 'String','Neuron Decisions', 'Position', [15 407 100 15]);
+
+%% Data table, initial figure, button formatting
+
+t.Data = d;
 t.ColumnName = {'Cluster','Included'};
 t.ColumnEditable = [false, true];
 t.ColumnWidth = {50, 50};
 t.RowName = [];
-t.Position(4) = pos(4) - 20 - button.Position(4);
+
+t.Position(4) = 2 * (pos(4) - 50 - button.Position(4));
 t.Position(3) = 119;
-t.Position(2) = t.Position(2) + 9;
-f.Position(3) = 157;
+t.Position(2) = button.Position(4) + 50;
+
+f.Position(3) = 157 * 5;
+f.Position(4) = 2 * pos(4);
+set(f, 'WindowStyle','docked');
+set(f, 'units', 'normalized', 'position',[0 0 1 1])
+
+button.Position(2) = button.Position(4);
+button.Position(3) = 119;
+
+%% Images to be displayed on home screen
+
+isi = axes('Units', 'Pixels','Position', [(t.Position(3)+100)  pos(4) 450 350]);
+imisi = imread([new_directory filesep 'Metrics' filesep 'isi__high_violators.png']);
+figure(f); image(imisi); axis tight; axis off;
+
+autocor = axes('Units', 'Pixels', 'Position', [(t.Position(3) + 100) 0 450 350]);
+imauto = imread([new_directory filesep 'Metrics' filesep 'pairwisecorr_0lag_sig.png']);
+figure(f); image(imauto); axis tight; axis off;
+
+%% Triangular matrix table and figure initialization and formatting 
+
+ftri = figure('name','Correlations Matrix');
+triang = TriangTable(clusters);
+
+tritab = uitable(ftri, 'Data', triang(2:end, :));
+set(ftri, 'WindowStyle', 'docked');
+tritab.RowName = triang(1,:);
+tritab.ColumnName = [];
+
+set(tritab, 'units', 'normalized', 'position', [0 0 1 1], 'CellSelectionCallback', @triSelect);
+
+%% Callback functions
 
     function callback(hObj,event)
         d = t.Data;
@@ -57,7 +98,53 @@ f.Position(3) = 157;
         end
         
         save(fullfile(new_directory,'final_clusters'), 'g')
-        close(f)
+        close all;
+    end
+
+    function cellSelect(hObj,event)
+        selected = t.Data(event.Indices(1), :);
+        c = selected{1};
+        dot = strfind(c, '.');
+        
+        if strcmp(wave_or_kilo, 'wave')
+            imfile = ['cluster_' c(1:dot-1) '_' c(dot+1:end) '.png'];
+            imsorted = imread([new_directory filesep 'WaveClus' filesep imfile]);
+        elseif strcmp(wave_or_kilo, 'kilo')
+            imfile = ['cluster_' c '.png'];
+            imsorted = imread([new_directory filesep 'KiloSort' filesep imfile]);
+        end
+        f1 = figure();
+        set(f1, 'WindowStyle','docked', 'name', 'Waveform');
+        figure(f1); image(imsorted); axis off; axis tight;
+        
+        imisidis = imread([new_directory filesep 'Metrics' filesep 'isi_distribution_' c]);
+        f2 = figure();
+        set(f2, 'WindowStyle','docked','name','ISI Distribution');
+        figure(f2); image(imisidis); axis off; axis tight;
+        
+        imaut = imread([new_directory filesep 'Metrics' filesep 'autocorrelations' filesep 'autocorr_cluster0' '.png']);
+        %imread([new_directory filesep 'Metrics' filesep 'autocorrelations' filesep 'autocorr_cluster' c '.png']);
+        f3 = figure();
+        set(f3, 'WindowStyle','docked','name','Autocorrelogram');
+        figure(f3); image(imaut); axis off; axis tight;
+    end 
+
+    function triSelect(hObj,event)
+        headings = cell2mat(triang(1, :));
+        
+        n1 = headings(event.Indices(1));
+        
+        n = find(headings == n1);
+        m = find(cell2mat(tritab.Data(1, :)) == cell2mat(tritab.Data(event.Indices(2))));
+        n2 = tritab.Data(n,m);
+        n2 = n2{1};
+        
+        imcor = imread([new_directory filesep 'Metrics' filesep ...
+            'cross_correlograms' filesep 'xcorr_0_2.png']);
+                %imread([new_directory filesep 'Metrics' filesep ...
+            %'cross_correlograms' filesep 'xcorr_' num2str(n1) '_' num2str(n2) '.png']);
+        fcor = figure('WindowStyle', 'docked', 'name', 'Cross Correlogram');
+        figure(fcor); image(imcor); axis off; axis tight;
     end
 
 end
