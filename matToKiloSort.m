8cf6b7e973618e0243c74ef0610c7d4796f4dbda
marsalad <marsalad@users.noreply.github.com>
2017-07-20 14:25:05 -0400

    graph clusters and save pre-merge rez file

--- matToKiloSort.m
+++ matToKiloSort.m
@@ -1,37 +1,51 @@
 function matToKiloSort(fbinary,fpath,num_channels)
-%TDTtoKiloSort  Run a KiloSort simulation using TDT data.
-%   TDTtoKiloSort(fname) runs KiloSort on datafile and saves to fpath
+%matToKiloSort  Run a KiloSort simulation using binary-converted TDT data.
+%   matToKiloSort(fbinary,fpath,num_channels) will run KiloSort on fbinary
+%   and save all data in fpath, including waveform graphs.
+%   num_channels is needed to specify the exact number of channels in the
+%   binary file.
+%
+%   matToKiloSort(fbinary,fpath) executes in the same manner as above, but
+%   will assume 64 channels.
 
+%% initialize variables
 if nargin < 3
     num_channels = 64;
 end
 
-% specify location to store data files for this simulation
-if ~exist(fpath, 'dir'); mkdir(fpath); end
-
 addpath(genpath('C:\work\KiloSort-master')) % path to kilosort folder
 addpath(genpath('C:\work\phy-master')) % path to npy-matlab scripts
 pathToYourConfigFile = 'D:\SpikeSortingPipeline\Code\matToKiloSort';
+pathToUnmerged = [fpath,'\NoMerge'];
+
+% specify location to store data files for this simulation
+if ~exist(fpath, 'dir'); mkdir(fpath); end
+if ~exist(pathToUnmerged, 'dir'); mkdir(pathToUnmerged); end
 
 % run the config file to build the structure of options (ops)
 run(fullfile(pathToYourConfigFile, 'config.m'))
+if mod(ops.Nfilt,32) ~= 0, ops.Nfilt = ops.Nfilt + 32 - mod(ops.Nfilt); end
 
 if ops.GPU, gpuDevice(1); end % initialize GPU
-tic; % start timer
 
 createChannelMapFile(fpath,num_channels) % create and save file in specified location
 
-% process formatted data with KiloSort
+%% process formatted data with KiloSort
 [rez, DATA, uproj] = preprocessData(ops); % preprocess data and extract spikes
 rez = fitTemplates(rez, DATA, uproj); % fit templates iteratively
-rez = fullMPMU(rez, DATA); % extract final spike times (overlapping extraction)
-rez = merge_posthoc2(rez); % use KiloSort's automerge functionality
+rez_nomerge = fullMPMU(rez, DATA); % extract final spike times (overlapping extraction)
+rez = merge_posthoc2(rez_nomerge); % use KiloSort's automerge functionality
 
+%% save files
+rezToPhy(rez_nomerge, pathToUnmerged); % save python results file for Phy
 rezToPhy(rez, fpath); % save python results file for Phy
+save(fullfile(pathToUnmerged,  'rez.mat'), 'rez', '-v7.3'); % save matlab results file
 save(fullfile(fpath,  'rez.mat'), 'rez', '-v7.3'); % save matlab results file
 delete(ops.fproc); % remove temporary file
 
+% save cluster plots
+graphSpikes(fpath,rez);
+
 % final display of total runtime
 fprintf('KiloSort took %2.2f minutes to run \n', floor(toc/0.6)/100)
-quit
 end
