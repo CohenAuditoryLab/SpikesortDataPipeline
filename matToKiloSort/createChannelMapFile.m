6fa8d153f792af2ecb8efacdaa5b4e6fa1872d49
marsalad <marsalad@users.noreply.github.com>
2017-07-24 10:39:32 -0400

    changed variable name

--- matToKiloSort/createChannelMapFile.m
+++ matToKiloSort/createChannelMapFile.m
@@ -1,18 +1,18 @@
-function createChannelMapFile(fpath,Nchannels)
+function createChannelMapFile(fpath,num_channels)
 %createChannelMapFile  Create a chanMap file for a KiloSort simulation.
 
-connected   = true(Nchannels, 1);
-chanMap     = 1:Nchannels;
+connected   = true(num_channels, 1);
+chanMap     = 1:num_channels;
 chanMap0ind = chanMap - 1;
 xcoords     = []; % all channels in same x-coord
-ycoords     = []; % channels spaced evenly along y-axis
-kcoords     = ones(1,Nchannels); % grouping of channels (i.e. tetrode groups)
+ycoords     = []; % all channels in same y-coord
+kcoords     = ones(1,num_channels); % grouping of channels (i.e. tetrode groups)
 
 %%%%
 % Form for running MultiBrush arrays, channel position is unkown
-% xcoords = zeros(nchannels,1);
-% ycoords = 50 * [1:nchannels];
-% kcoords = ones(nchannels,1);
+% xcoords = zeros(num_channels,1);
+% ycoords = 50 * [1:num_channels];
+% kcoords = ones(num_channels,1);
 %%%%
 
 save(fullfile(fpath, 'chanMap.mat'), ...
