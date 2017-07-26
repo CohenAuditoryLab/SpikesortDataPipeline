0b089ffc8a563eda4c9d0f9de709801d36592cfa
marsalad <marsalad@users.noreply.github.com>
2017-07-26 10:19:55 -0400

    comments

--- TDTconverter/convertingtanks_16channels.m
+++ TDTconverter/convertingtanks_16channels.m
@@ -1,19 +1,17 @@
-%[~,~,raw]=xlsread('C:\work\Penn\sorting\SAM_master_list');
-load('D:\SpikeSortingPipeline\Code\TDTconverter\SAM_master_list.mat');
-% task={'STRF','STRF5m','VOC'};
+[~,~,raw]=xlsread('D:\SpikeSortingPipeline\Tanks\SAM_master_list'); %this reads excel master list
+%Diana will create sorting file index
 task={'Textures'};
 datapath='D:\SpikeSortingPipeline\Tanks\';
 outpath='D:\SpikeSortingPipeline\ToSort\';
 %XPZ5->ML
 %XPZ2->AL
-for exp=18:27
+for exp=18:27 % goes through all experiments using the correlative from master list
     index=find(cell2mat(raw(:,1))==exp);
  for taskindex=1:length(index)
     index2=find(strcmp([raw(index,7)],task{taskindex})==1);
     tankfile=char(raw(index(index2),4));
     blocknum=cell2mat(raw(index(index2),6));
     numchannels=cell2mat(raw(index(index2),8));
-    
     for z=1:6
         %AL
         outp=[outpath tankfile '_AL'];
