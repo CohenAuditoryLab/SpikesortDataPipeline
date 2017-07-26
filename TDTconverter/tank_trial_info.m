45ec7d610601119a1831da5d365ab76eb217f4e5
marsalad <marsalad@sas.upenn.edu>
2017-07-26 10:47:54 -0400

    formatting changes

--- TDTconverter/tank_trial_info.m
+++ TDTconverter/tank_trial_info.m
@@ -13,16 +13,15 @@ function meta=tank_trial_info(tank_path,block_number)
 %           sound_stimuli : sound being played to the speakers
 %           triggers      : If Dynamic Moving Ripple or vocalizations are being played this waveform will have triggers
 
-
-full_path=fullfile(tank_path,['Block-' num2str(block_number)]); %So you are happy I'm using fullfile.....
+full_path=fullfile(tank_path,['Block-' num2str(block_number)]);
 temp=SEV2mat(full_path,'Channel',131,'EVENTNAME','xpz2');
-meta.reward=temp.xpz2.data;clear temp
+meta.reward=temp.xpz2.data; clear temp;
 temp=SEV2mat(full_path,'Channel',132,'EVENTNAME','xpz2');
-meta.joystick=temp.xpz2.data;clear temp
+meta.joystick=temp.xpz2.data; clear temp;
 temp=SEV2mat(full_path,'Channel',134,'EVENTNAME','xpz2');
-meta.trial_on=temp.xpz2.data;clear temp
+meta.trial_on=temp.xpz2.data; clear temp;
 temp=SEV2mat(full_path,'Channel',65,'EVENTNAME','xpz5');
-meta.sound_stimuli =temp.xpz5.data;clear temp
+meta.sound_stimuli =temp.xpz5.data; clear temp;
 temp=SEV2mat(full_path,'Channel',66,'EVENTNAME','xpz5');
-meta.triggers =temp.xpz5.data;clear temp
+meta.triggers =temp.xpz5.data; clear temp;
 end
