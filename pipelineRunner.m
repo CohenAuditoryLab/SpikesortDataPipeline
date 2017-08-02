44378842a0a2aa36dbe35f080bdcee345873fed5
marsalad <marsalad@users.noreply.github.com>
2017-08-02 15:06:55 -0400

    removed WaveClus

--- pipelineRunner.m
+++ pipelineRunner.m
@@ -9,18 +9,13 @@ convertTanks(origin,temp);
 % spike sorting
 files = dir(temp);
 files = files([files.isdir]);
+files = files(3:end);
 for i = 1:length(files)
-    move(fullfile(temp,files(i)),dest);
-    
-    % run KiloSort and WaveClus
-    matToKiloSort(fullfile(dest,files(i),[files(i) '.dat']), ...
-        fullfile(dest,files(i),'KiloSort'), 96);
-    matToWaveClus(fullfile(dest,files(i),[files(i) '.dat']), ...
-        fullfile(dest,files(i),'WaveClus'), 96);
-    
-    % run metrics
-    sorting_metrics(fullfile(dest,files(i),'KiloSort'), ...
-        'kilo', fullfile(dest,files(i),'Metrics','KiloSort'));
-    sorting_metrics(fullfile(dest,files(i),'WaveClus','all_clusters.dat'), ...
-        'wave', fullfile(dest,files(i),'Metrics','WaveClus'));
+    f = files(i).name;
+    movefile(fullfile(temp,f),dest);
+
+    matToKiloSort(fullfile(dest,f,[f '.dat']), ...
+        fullfile(dest,f,'KiloSort'), 96);
+    sorting_metrics(fullfile(dest,f,'KiloSort'), ...
+        'kilo', fullfile(dest,f,'Metrics','KiloSort'));
 end
