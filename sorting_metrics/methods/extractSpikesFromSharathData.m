34105b984cb5772719942e0f430538be6a2a9121
mattschaff <matthew.schaff@gmail.com>
2018-07-19 12:31:39 -0400

    made extractSpikesFromSharathData.m easier to use

--- sorting_metrics/methods/extractSpikesFromSharathData.m
+++ sorting_metrics/methods/extractSpikesFromSharathData.m
@@ -3,16 +3,13 @@ function [spike_times, spike_clusters] = extractSpikesFromSharathData(data_file)
     % OUTPUT
         % spike_times => vector of spike times in ms
         % spike_clusters => neuron ID assignments of spikes
+    % initialize variables
     bin_length = 1;
     trial_length = 2000;
     CohenNeurons = load(data_file);
     CohenNeurons = CohenNeurons.CohenNeurons;
-    % Number of neurons
     num_neurons = 16;
     num_trials = 464;
-    % Pre-define binranges from 0 to 2000ms
-    binranges = linspace(0,trial_length,trial_length/bin_length+1);
-    % For each neuron...
     all_spikes_labels_array = [];
     all_spikes_labels_array = reshape(all_spikes_labels_array, [0,2]);
     for i = 1:num_neurons
@@ -20,10 +17,14 @@ function [spike_times, spike_clusters] = extractSpikesFromSharathData(data_file)
         spikes_array = [];
         spikes_array = reshape(spikes_array, [0,1]);
         for j = 1:num_trials
+            % add to array spikes from this neuron & this trial
             spikes_array = vertcat(spikes_array, CohenNeurons(i).trials(j).spikes.');
+            % add to array the ID of the neuron
             label = zeros(size(spikes_array,1), 1); label(:) = CohenNeurons(i).ID;
+            % combine IDs & spikes
             spikes_labels_array = horzcat(spikes_array, label);
         end
+        % collect all neuron ID & spikes into one array
         all_spikes_labels_array = vertcat(all_spikes_labels_array, spikes_labels_array);
     end
     spike_times = all_spikes_labels_array(:,1);
